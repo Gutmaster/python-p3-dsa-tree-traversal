34d1b9dc366cc2d149dcdcda010b68b855c26615
Chevy <vallchevy@gmail.com>
2024-06-23 15:29:43 -0300

    complete lab

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,26 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    return self.depth_first_recursive(self.root, id)
+
+  def depth_first_recursive(self, node, id):
+    for child in node['children']:
+      if child['id'] == id:
+        return child
+      else:
+        return self.depth_first_recursive(child, id)
+      
+
+  def breadth_first_search(self, node, id):
+    nodes_to_visit = [node]
+
+    while len(nodes_to_visit) > 0:
+      # 1. Remove the first node from the `nodes_to_visit` list
+      node = nodes_to_visit.pop(0)
+      # 2. Check to see if it's the node we're searching for
+      if node['id'] == id:
+        return node
+      # 3. Add its children (if any) to the END of the `nodes_to_visit` list
+      nodes_to_visit = nodes_to_visit + node['children']
+
+    return None
